6bb74add2b3ef6f32fdbdafeb8fd17a00ef8e869
Jeong Jingyo <sqidward1208@naver.com>
2023-11-04 12:10:02 +0900

    9012v2

--- 9012.py
+++ 9012.py
@@ -9,20 +9,20 @@ def main(len_queue):
     main(len_queue - 1)
 
 def solve(ps: str) -> bool:
-    stack = []
+    stack = 0
     return next(ps, stack)
 
-def next(part: str, stack: list[None]) -> bool:
-    if len(part) == 0 and len(stack) == 0: return True
+def next(part: str, stack: int) -> bool:
+    if len(part) == 0 and stack == 0: return True
     elif len(part) == 0: return False
 
     if part[0] == "(":
-        stack.append(None)
+        stack = stack + 1
     elif part[0] == ")":
-        if len(stack) == 0:
+        if stack == 0:
             return False
         else:
-            stack.pop()
+            stack = stack - 1
     return next(part[1:], stack)
 
 if __name__ == "__main__":
