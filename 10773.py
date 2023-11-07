55ca62be19c36482a0b409305638304cd4a547b3
Jeong Jingyo <sqidward1208@naver.com>
2023-11-07 19:58:23 +0900

    10773v3
    loop -> recursion
        => 68ms -> 1936ms

--- 10773.py
+++ 10773.py
@@ -1,23 +1,34 @@
 import sys
+sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
 
 def main():
     k = int(input())
-    queue = []
+    cmd_queue = []
     for _ in range(k):
-        cmd = int(input())
+        cmd_queue.append(int(input()))
 
-        if cmd == 0:
-            queue.pop()
-        else:
-            queue.append(cmd)
-
+    result = process_next(cmd_queue)
     answer = 0
-    for i in queue:
+
+    for i in result:
         answer = answer + i
 
     return answer
+
+
+def process_next(cmd_queue: list[int], numbers: list[int] = []):
+    if len(cmd_queue) == 0:
+        return numbers
+
+    cmd = cmd_queue.pop(0)
+    if (cmd == 0):
+        numbers.pop()
+    else:
+        numbers.append(cmd)
+
+    return process_next(cmd_queue, numbers)
 
 
 if __name__ == "__main__":
