49fbcff9bac60c25b410e10718ff45cf22e705e1
Jeong Jingyo <sqidward1208@naver.com>
2023-11-07 16:10:57 +0900

    10773

---
+++ 10773.py
@@ -0,0 +1,20 @@
+def main():
+    k = int(input())
+    queue = []
+    for _ in range(k):
+        cmd = int(input())
+
+        if cmd == 0:
+            queue.pop()
+        else:
+            queue.append(cmd)
+
+    answer = 0
+    for i in queue:
+        answer = answer + i
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(main())
