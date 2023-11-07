5c5444fdd25aa790bde82458f4373c1f62493794
Jeong Jingyo <sqidward1208@naver.com>
2023-11-07 20:24:48 +0900

    10773v4
    재귀식 내에서 sys.stdin.readline
    확실히 반복보다 재귀가 느림(68ms -> 144ms)

--- 10773.py
+++ 10773.py
@@ -5,11 +5,8 @@ input = sys.stdin.readline
 
 def main():
     k = int(input())
-    cmd_queue = []
-    for _ in range(k):
-        cmd_queue.append(int(input()))
+    result = process_next(k)
 
-    result = process_next(cmd_queue)
     answer = 0
 
     for i in result:
@@ -18,17 +15,17 @@ def main():
     return answer
 
 
-def process_next(cmd_queue: list[int], numbers: list[int] = []):
-    if len(cmd_queue) == 0:
+def process_next(k, numbers: list[int] = []):
+    if k == 0:
         return numbers
 
-    cmd = cmd_queue.pop(0)
+    cmd = int(input())
     if (cmd == 0):
         numbers.pop()
     else:
         numbers.append(cmd)
 
-    return process_next(cmd_queue, numbers)
+    return process_next(k - 1, numbers)
 
 
 if __name__ == "__main__":
